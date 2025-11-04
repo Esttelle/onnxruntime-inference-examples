66196c3a9b7aad8b81b099c8c67dce8c03dd390f
Esttelle <18220760769@163.com>
2025-11-05 00:25:26 +0800

    update 11.4 modifications

--- test_bert_torch1.py
+++ test_bert_torch1.py
@@ -32,42 +32,29 @@
 
 import logging
 import os
-import random
 import sys
 import torch
-from torchao.quantization import quantize_, Int8DynamicActivationInt8WeightConfig
 from collections import Counter
-from dataclasses import dataclass, field
-from typing import Optional
+from argparse import Namespace
 
-import datasets
 import evaluate
 import numpy as np
 from datasets import load_dataset
 
-import transformers
 from transformers import (
-    AutoConfig,
-    AutoModelForSequenceClassification,
-    AutoTokenizer,
-    DataCollatorWithPadding,
     EvalPrediction,
-    HfArgumentParser,
     PretrainedConfig,
     Trainer,
     TrainingArguments,
     default_data_collator,
     set_seed,
 )
-from transformers.trainer_utils import get_last_checkpoint
+from transformers import (BertForSequenceClassification, BertTokenizer,)
 from transformers.utils import check_min_version
-from transformers.utils.versions import require_version
 
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.57.0")
-
-require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/text-classification/requirements.txt")
 
 task_to_keys = {
     "cola": ("sentence", None),
@@ -83,176 +70,32 @@ task_to_keys = {
 
 logger = logging.getLogger(__name__)
 
+configs = Namespace()
 
-@dataclass
-class DataTrainingArguments:
-    """
-    Arguments pertaining to what data we are going to input our model for training and eval.
-
-    Using `HfArgumentParser` we can turn this class
-    into argparse arguments to be able to specify them on
-    the command line.
-    """
-
-    task_name: Optional[str] = field(
-        default=None,
-        metadata={"help": "The name of the task to train on: " + ", ".join(task_to_keys.keys())},
-    )
-    dataset_name: Optional[str] = field(
-        default=None, metadata={"help": "The name of the dataset to use (via the datasets library)."}
-    )
-    dataset_config_name: Optional[str] = field(
-        default=None, metadata={"help": "The configuration name of the dataset to use (via the datasets library)."}
-    )
-    max_seq_length: int = field(
-        default=128,
-        metadata={
-            "help": (
-                "The maximum total input sequence length after tokenization. Sequences longer "
-                "than this will be truncated, sequences shorter will be padded."
-            )
-        },
-    )
-    overwrite_cache: bool = field(
-        default=False, metadata={"help": "Overwrite the cached preprocessed datasets or not."}
-    )
-    pad_to_max_length: bool = field(
-        default=True,
-        metadata={
-            "help": (
-                "Whether to pad all samples to `max_seq_length`. "
-                "If False, will pad the samples dynamically when batching to the maximum length in the batch."
-            )
-        },
-    )
-    max_train_samples: Optional[int] = field(
-        default=None,
-        metadata={
-            "help": (
-                "For debugging purposes or quicker training, truncate the number of training examples to this "
-                "value if set."
-            )
-        },
-    )
-    max_eval_samples: Optional[int] = field(
-        default=None,
-        metadata={
-            "help": (
-                "For debugging purposes or quicker training, truncate the number of evaluation examples to this "
-                "value if set."
-            )
-        },
-    )
-    max_predict_samples: Optional[int] = field(
-        default=None,
-        metadata={
-            "help": (
-                "For debugging purposes or quicker training, truncate the number of prediction examples to this "
-                "value if set."
-            )
-        },
-    )
-    train_file: Optional[str] = field(
-        default=None, metadata={"help": "A csv or a json file containing the training data."}
-    )
-    validation_file: Optional[str] = field(
-        default=None, metadata={"help": "A csv or a json file containing the validation data."}
-    )
-    test_file: Optional[str] = field(default=None, metadata={"help": "A csv or a json file containing the test data."})
-
-    def __post_init__(self):
-        if self.task_name is not None:
-            self.task_name = self.task_name.lower()
-            if self.task_name not in task_to_keys:
-                raise ValueError("Unknown task, you should pick one in " + ",".join(task_to_keys.keys()))
-        elif self.dataset_name is not None:
-            pass
-        elif self.train_file is None or self.validation_file is None:
-            raise ValueError("Need either a GLUE task, a training/validation file or a dataset name.")
-        else:
-            train_extension = self.train_file.split(".")[-1]
-            assert train_extension in ["csv", "json"], "`train_file` should be a csv or a json file."
-            validation_extension = self.validation_file.split(".")[-1]
-            assert validation_extension == train_extension, (
-                "`validation_file` should have the same extension (csv or json) as `train_file`."
-            )
-
-
-@dataclass
-class ModelArguments:
-    """
-    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
-    """
-
-    model_name_or_path: str = field(
-        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
-    )
-    config_name: Optional[str] = field(
-        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
-    )
-    tokenizer_name: Optional[str] = field(
-        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
-    )
-    cache_dir: Optional[str] = field(
-        default=None,
-        metadata={"help": "Where do you want to store the pretrained models downloaded from huggingface.co"},
-    )
-    use_fast_tokenizer: bool = field(
-        default=True,
-        metadata={"help": "Whether to use one of the fast tokenizer (backed by the tokenizers library) or not."},
-    )
-    model_revision: str = field(
-        default="main",
-        metadata={"help": "The specific model version to use (can be a branch name, tag name or commit id)."},
-    )
-    token: str = field(
-        default=None,
-        metadata={
-            "help": (
-                "The token to use as HTTP bearer authorization for remote files. If not specified, will use the token "
-                "generated when running `hf auth login` (stored in `~/.huggingface`)."
-            )
-        },
-    )
-    trust_remote_code: bool = field(
-        default=False,
-        metadata={
-            "help": (
-                "Whether to trust the execution of code from datasets/models defined on the Hub."
-                " This option should only be set to `True` for repositories you trust and in which you have read the"
-                " code, as it will execute code present on the Hub on your local machine."
-            )
-        },
-    )
-    ignore_mismatched_sizes: bool = field(
-        default=False,
-        metadata={"help": "Will enable to load a pretrained model whose head dimensions are different."},
-    )
-
-output_dir = "./MRPC/"
+# The output directory for the fine-tuned model, $OUT_DIR.
+configs.output_dir = "./MRPC/"
 
 # The data directory for the MRPC task in the GLUE benchmark, $GLUE_DIR/$TASK_NAME.
-data_dir = "./glue_data/MRPC"
+configs.data_dir = "./glue_data/MRPC"
+
+# The model name or path for the pre-trained model.
+configs.model_name_or_path = "bert-base-uncased"
+# The maximum length of an input sequence
+configs.max_seq_length = 128
 
 # Prepare GLUE task.
-task_name = "MRPC".lower()
-model_name_or_path="bert-base-uncased"
-max_seq_length=128
+configs.task_name = "MRPC".lower()
+configs.device = "cpu"
 
-cache_dir = os.path.join(data_dir, 'cached_eval_{}_{}_{}'.format(
-    list(filter(None, model_name_or_path.split('/'))).pop(),
-    str(max_seq_length),
-    str(task_name)))
-
-model_args = ModelArguments(model_name_or_path=model_name_or_path,
-                            cache_dir=cache_dir)
-
-data_args = DataTrainingArguments(task_name=task_name,
-                                    max_seq_length=max_seq_length)
+# Where do you want to store the pretrained models downloaded from huggingface.co
+configs.cache_dir = os.path.join(configs.data_dir, 'cached_eval_{}_{}_{}'.format(
+    list(filter(None, configs.model_name_or_path.split('/'))).pop(),
+    str(configs.max_seq_length),
+    str(configs.task_name)))
 
 training_args = TrainingArguments(
-    output_dir=output_dir,
-    do_eval=True)
+    output_dir=configs.output_dir,
+    do_eval=True, use_cpu=True)
 
 # Setup logging
 logging.basicConfig(
@@ -260,17 +103,6 @@ logging.basicConfig(
     datefmt="%m/%d/%Y %H:%M:%S",
     handlers=[logging.StreamHandler(sys.stdout)],
 )
-
-if training_args.should_log:
-    # The default of training_args.log_level is passive, so we set log level at info here to have that default.
-    transformers.utils.logging.set_verbosity_info()
-
-log_level = training_args.get_process_log_level()
-logger.setLevel(log_level)
-datasets.utils.logging.set_verbosity(log_level)
-transformers.utils.logging.set_verbosity(log_level)
-transformers.utils.logging.enable_default_handler()
-transformers.utils.logging.enable_explicit_format()
 
 # Log on each process the small summary:
 logger.warning(
@@ -283,22 +115,27 @@ logger.info(f"Training/evaluation parameters {training_args}")
 set_seed(training_args.seed)
 
 
-def evaluate_model(model, config, tokenizer, raw_datasets, num_labels, is_regression, label_list):
+def evaluate_model(model, tokenizer):
     # Preprocessing the raw_datasets
-    sentence1_key, sentence2_key = task_to_keys[data_args.task_name]
+    raw_datasets = load_dataset("glue", configs.task_name)
+
+    # Labels
+    is_regression = configs.task_name == "stsb"
+    if not is_regression:
+        label_list = raw_datasets["train"].features["label"].names
+        num_labels = len(label_list)
+    else:
+        num_labels = 1
+        
+    sentence1_key, sentence2_key = task_to_keys[configs.task_name]
 
     # Padding strategy
-    if data_args.pad_to_max_length:
-        padding = "max_length"
-    else:
-        # We will pad later, dynamically at batch creation, to the max sequence length in each batch
-        padding = False
+    padding = "max_length"
 
     # Some models have set the order of the labels to use, so let's make sure we do use it.
     label_to_id = None
     if (
         model.config.label2id != PretrainedConfig(num_labels=num_labels).label2id
-        and data_args.task_name is not None
         and not is_regression
     ):
         # Some have all caps in their config, some don't.
@@ -311,22 +148,20 @@ def evaluate_model(model, config, tokenizer, raw_datasets, num_labels, is_regres
                 f"model labels: {sorted(label_name_to_id.keys())}, dataset labels: {sorted(label_list)}."
                 "\nIgnoring the model labels as a result.",
             )
-    elif data_args.task_name is None and not is_regression:
-        label_to_id = {v: i for i, v in enumerate(label_list)}
 
     if label_to_id is not None:
         model.config.label2id = label_to_id
-        model.config.id2label = {id: label for label, id in config.label2id.items()}
-    elif data_args.task_name is not None and not is_regression:
+        model.config.id2label = {id: label for label, id in model.config.label2id.items()}
+    elif not is_regression:
         model.config.label2id = {l: i for i, l in enumerate(label_list)}
-        model.config.id2label = {id: label for label, id in config.label2id.items()}
+        model.config.id2label = {id: label for label, id in model.config.label2id.items()}
 
-    if data_args.max_seq_length > tokenizer.model_max_length:
+    if configs.max_seq_length > tokenizer.model_max_length:
         logger.warning(
-            f"The max_seq_length passed ({data_args.max_seq_length}) is larger than the maximum length for the "
+            f"The max_seq_length passed ({configs.max_seq_length}) is larger than the maximum length for the "
             f"model ({tokenizer.model_max_length}). Using max_seq_length={tokenizer.model_max_length}."
         )
-    max_seq_length = min(data_args.max_seq_length, tokenizer.model_max_length)
+    max_seq_length = min(configs.max_seq_length, tokenizer.model_max_length)
 
     def preprocess_function(examples):
         # Tokenize the texts
@@ -343,7 +178,7 @@ def evaluate_model(model, config, tokenizer, raw_datasets, num_labels, is_regres
     raw_datasets = raw_datasets.map(
         preprocess_function,
         batched=True,
-        load_from_cache_file=not data_args.overwrite_cache,
+        load_from_cache_file=True,
         desc="Running tokenizer on dataset",
     )
 
@@ -356,14 +191,11 @@ def evaluate_model(model, config, tokenizer, raw_datasets, num_labels, is_regres
 
     if "validation" not in raw_datasets and "validation_matched" not in raw_datasets:
         raise ValueError("--do_eval requires a validation dataset")
-    eval_dataset = raw_datasets["validation_matched" if data_args.task_name == "mnli" else "validation"]
-    if data_args.max_eval_samples is not None:
-        max_eval_samples = min(len(eval_dataset), data_args.max_eval_samples)
-        eval_dataset = eval_dataset.select(range(max_eval_samples))
+    eval_dataset = raw_datasets["validation_matched" if configs.task_name == "mnli" else "validation"]
     print_class_distribution(eval_dataset, "validation")
 
     # Get the metric function
-    metric = evaluate.load("glue", data_args.task_name, cache_dir=model_args.cache_dir)
+    metric = evaluate.load("glue", configs.task_name, cache_dir=configs.cache_dir)
 
     # You can define your custom compute_metrics function. It takes an `EvalPrediction` object (a namedtuple with a
     # predictions and label_ids field) and has to return a dictionary string to float.
@@ -381,12 +213,7 @@ def evaluate_model(model, config, tokenizer, raw_datasets, num_labels, is_regres
 
     # Data collator will default to DataCollatorWithPadding when the tokenizer is passed to Trainer, so we change it if
     # we already did the padding.
-    if data_args.pad_to_max_length:
-        data_collator = default_data_collator
-    elif training_args.fp16:
-        data_collator = DataCollatorWithPadding(tokenizer, pad_to_multiple_of=8)
-    else:
-        data_collator = None
+    data_collator = default_data_collator
 
     # Initialize our Trainer
     trainer = Trainer(
@@ -403,24 +230,19 @@ def evaluate_model(model, config, tokenizer, raw_datasets, num_labels, is_regres
     logger.info("*** Evaluate ***")
 
     # Loop to handle MNLI double evaluation (matched, mis-matched)
-    tasks = [data_args.task_name]
+    tasks = [configs.task_name]
     eval_datasets = [eval_dataset]
-    if data_args.task_name == "mnli":
+    if configs.task_name == "mnli":
         tasks.append("mnli-mm")
         valid_mm_dataset = raw_datasets["validation_mismatched"]
-        if data_args.max_eval_samples is not None:
-            max_eval_samples = min(len(valid_mm_dataset), data_args.max_eval_samples)
-            valid_mm_dataset = valid_mm_dataset.select(range(max_eval_samples))
         eval_datasets.append(valid_mm_dataset)
         combined = {}
 
     for eval_dataset, task in zip(eval_datasets, tasks):
         metrics = trainer.evaluate(eval_dataset=eval_dataset)
 
-        max_eval_samples = (
-            data_args.max_eval_samples if data_args.max_eval_samples is not None else len(eval_dataset)
-        )
-        metrics["eval_samples"] = min(max_eval_samples, len(eval_dataset))
+        max_eval_samples = len(eval_dataset)
+        metrics["eval_samples"] = len(eval_dataset)
 
         if task == "mnli-mm":
             metrics = {k + "_mm": v for k, v in metrics.items()}
@@ -428,86 +250,45 @@ def evaluate_model(model, config, tokenizer, raw_datasets, num_labels, is_regres
             combined.update(metrics)
 
         trainer.log_metrics("eval", metrics)
-        trainer.save_metrics("eval", combined if task is not None and "mnli" in task else metrics)
 
-    kwargs = {"finetuned_from": model_args.model_name_or_path, "tasks": "text-classification"}
-    if data_args.task_name is not None:
-        kwargs["language"] = "en"
-        kwargs["dataset_tags"] = "glue"
-        kwargs["dataset_args"] = data_args.task_name
-        kwargs["dataset"] = f"GLUE {data_args.task_name.upper()}"
-
-    trainer.create_model_card(**kwargs)
 
 def print_size_of_model(model):
     torch.save(model.state_dict(), "temp.p")
     print('Size (MB):', os.path.getsize("temp.p")/(1024*1024))
     os.remove('temp.p')
 
+
 def main():
     # Load pretrained model and tokenizer
     #
     # In distributed training, the .from_pretrained methods guarantee that only one local process can concurrently
     # download model & vocab.
-    raw_datasets = load_dataset(
-        "glue",
-        data_args.task_name,
-        cache_dir=model_args.cache_dir,
-        token=model_args.token,
-    )
 
-    # Labels
-    is_regression = data_args.task_name == "stsb"
-    if not is_regression:
-        label_list = raw_datasets["train"].features["label"].names
-        num_labels = len(label_list)
-    else:
-        num_labels = 1
+    # config = AutoConfig.from_pretrained(
+    #     configs.model_name_or_path,
+    #     num_labels=num_labels,
+    #     finetuning_task=configs.task_name,
+    #     cache_dir=configs.cache_dir,
+    #     revision="main",
+    #     token=None,
+    #     trust_remote_code=False
+    # )
+    # print(f"Config: {config}")
+    tokenizer = BertTokenizer.from_pretrained(configs.output_dir)
 
-    config = AutoConfig.from_pretrained(
-        model_args.config_name if model_args.config_name else model_args.model_name_or_path,
-        num_labels=num_labels,
-        finetuning_task=data_args.task_name,
-        cache_dir=model_args.cache_dir,
-        revision=model_args.model_revision,
-        token=model_args.token,
-        trust_remote_code=model_args.trust_remote_code
-    )
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
-        cache_dir=model_args.cache_dir,
-        use_fast=model_args.use_fast_tokenizer,
-        revision=model_args.model_revision,
-        token=model_args.token,
-        trust_remote_code=model_args.trust_remote_code
-    )
-    model = AutoModelForSequenceClassification.from_pretrained(
-        model_args.model_name_or_path,
-        from_tf=bool(".ckpt" in model_args.model_name_or_path),
-        config=config,
-        cache_dir=model_args.cache_dir,
-        revision=model_args.model_revision,
-        token=model_args.token,
-        trust_remote_code=model_args.trust_remote_code,
-        ignore_mismatched_sizes=model_args.ignore_mismatched_sizes
-    )
+    model = BertForSequenceClassification.from_pretrained(configs.output_dir)
+    model.to(configs.device)
 
-    evaluate_model(model, config, tokenizer, raw_datasets,  num_labels, is_regression, label_list)
+    evaluate_model(model, tokenizer)
 
     print_size_of_model(model)
-    # quantize model
-    # check_quantization_success(model)
 
-    quantize_(model, Int8DynamicActivationInt8WeightConfig())
-    quantized_model = torch.compile(model)
+    quantized_model = torch.ao.quantization.quantize_dynamic(
+        model, {torch.nn.Linear}, dtype=torch.qint8
+    )
 
     print_size_of_model(quantized_model)
-    evaluate_model(quantized_model, config, tokenizer, raw_datasets,  num_labels, is_regression, label_list)
-
-
-def _mp_fn(index):
-    # For xla_spawn (TPUs)
-    main()
+    evaluate_model(quantized_model, tokenizer)
 
 
 if __name__ == "__main__":
